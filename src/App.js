852222d20c04e2fb166958484285915c2f8a768d
draganasavanovic <dragana.savanovic@wayseven.com>
2020-04-23 10:32:29 +0200

    creating a functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi, I'm a React app</h1>
+      <Person></Person>
     </div>
   );
+  // return React.createElement(
+  //   "div",
+  //   { className: "App" },
+  //   React.createElement("h1", null, "Hi, I'm a React app")
+  // );
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const person = () => {
+  return <p>I am person</p>;
+};
+
+export default person;
