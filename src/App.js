5d0380a1d7a5b4bacfdce2b9fbf2e59450370805
draganasavanovic <dragana.savanovic@wayseven.com>
2020-04-23 15:12:51 +0200

    using the useState() hook for state manipulation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-function App() {
+const App = (props) => {
+  const [perosnsState, setPersonsState] = useState({
+    persons: [
+      { name: "Dragana", age: 27 },
+      { name: "Mladen", age: 31 },
+    ],
+    otherState: "some other value",
+  });
+
+  const [otherState, setOtherState] = useState({
+    otherState: "some other value",
+  });
+
+  console.log(perosnsState, otherState);
+
+  const switchNameHandler = () => {
+    console.log("clicked");
+    // this.state.persons[0].name = "Gaga";
+    setPersonsState({
+      persons: [
+        { name: "Gaga", age: 27 },
+        { name: "Mladjo", age: 31 },
+      ],
+      // otherState: perosnsState.otherState,
+    });
+  };
+
   return (
     <div className="App">
-      <Person name="Dragana" age="27" />
-      <Person name="Mladen" age="31">
+      <Person
+        name={perosnsState.persons[0].name}
+        age={perosnsState.persons[0].age}
+      />
+      <Person
+        name={perosnsState.persons[1].name}
+        age={perosnsState.persons[1].age}
+      >
         My Hobbies: Racing
       </Person>
+      <button onClick={switchNameHandler}>Switch Name</button>
     </div>
   );
   // return React.createElement(
@@ -16,6 +49,6 @@ function App() {
   //   { className: "App" },
   //   React.createElement("h1", null, "Hi, I'm a React app")
   // );
-}
+};
 
 export default App;
