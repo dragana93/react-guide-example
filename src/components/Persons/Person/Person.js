d046a621263c6a0efe4dd822371e1782126eef7a
draganasavanovic <dragana.savanovic@wayseven.com>
2020-05-07 15:31:28 +0200

    component creation lifecycle (for props changes)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Person.css";
 import styled from "styled-components";
 
@@ -15,23 +15,24 @@ const StyleDiv = styled.div`
   }
 `;
 
-const person = (props) => {
-  // const style = {
-  //   "@media(min-width: 500px)": {
-  //     width: "450px",
-  //   },
-  // };
-  console.log("[Person.js] rendering...");
-  return (
-    // <div className="Person" style={style}>
-    <StyleDiv>
-      <p onClick={props.click}>
-        I am {props.name} and I am {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </StyleDiv>
-  );
-};
+class Person extends Component {
+  render() {
+    console.log("[Person.js] rendering...");
+    return (
+      // <div className="Person" style={style}>
+      <StyleDiv>
+        <p onClick={this.props.click}>
+          I am {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </StyleDiv>
+    );
+  }
+}
 
-export default person;
+export default Person;
