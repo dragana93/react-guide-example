0a5be6cb515f0f093bf96a700d67fe68ecb34f93
draganasavanovic <dragana.savanovic@wayseven.com>
2020-05-14 13:59:50 +0200

    using refs in functional components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,32 +1,47 @@
 import React, { Component } from "react";
 import "./Person.css";
-import Auxiliary from "../../../hoc/Auxiliary";
-import styled from "styled-components";
+import PropTypes from "prop-types";
+// import Auxiliary from "../../../hoc/Auxiliary";
+// import styled from "styled-components";
 
-const StyleDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
+// const StyleDiv = styled.div`
+//   width: 60%;
+//   margin: 16px auto;
+//   border: 1px solid #eee;
+//   box-shadow: 0 2px 3px #ccc;
+//   padding: 16px;
+//   text-align: center;
 
-  @media (min-width: 500px) {
-    width: 450px;
-  }
-`;
+//   @media (min-width: 500px) {
+//     width: 450px;
+//   }
+// `;
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+  componentDidMount() {
+    // document.querySelector("input").focus();
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
   render() {
     console.log("[Person.js] rendering...");
     return (
       // <div className="Person" style={style}>
       <React.Fragment>
-        <p onClick={this.props.click}>
+        <p key="i1" onClick={this.props.click}>
           I am {this.props.name} and I am {this.props.age} years old!
         </p>
-        <p>{this.props.children}</p>
+        <p key="i2">{this.props.children}</p>
         <input
+          // ref={(inputEl) => {
+          //   this.inputElement = inputEl;
+          // }}
+          ref={this.inputElementRef}
+          key="i3"
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
@@ -36,4 +51,10 @@ class Person extends Component {
   }
 }
 
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func,
+};
 export default Person;
